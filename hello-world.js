feb7607e1a052679604eb1e9c1e2f88b8be15e84
CHANDRAN <sridhar.d.chandran@accenture.com>
2024-04-06 15:56:23 +0530

    first commit

--- hello-world.js
+++ hello-world.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer((req,res) => {
     console.dir(req, {depth:0})
-    res.write('Hello World.....')
+    res.write('Hello World next publish.....')
     res.end()
 })
 
